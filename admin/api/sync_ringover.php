fe27436d8f53223210412b8788a18a034e64d700
Marcos Morales <102313951+merlintxu@users.noreply.github.com>
2025-07-23 18:43:56 +0200

    modified:   admin/api/sync_ringover.php

--- admin/api/sync_ringover.php
+++ admin/api/sync_ringover.php
@@ -1,8 +1,9 @@
+
 <?php
+declare(strict_types=1);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-declare(strict_types=1);
 
 require_once __DIR__ . '/../auth.php';
 requireApiAuth();
