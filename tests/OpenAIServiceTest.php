f842ca637eb26447846a14b1f9e2f7428f90d8db
Marcos Morales <102313951+merlintxu@users.noreply.github.com>
2025-08-01 10:25:11 +0000

    feat: configure OpenAI model

--- tests/OpenAIServiceTest.php
+++ tests/OpenAIServiceTest.php
@@ -2,6 +2,7 @@
 namespace Tests;
 
 use FlujosDimension\Services\OpenAIService;
+use FlujosDimension\Core\Config;
 use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Handler\MockHandler;
@@ -20,7 +21,8 @@ class OpenAIServiceTest extends TestCase
         $stack = HandlerStack::create($mock);
         $stack->push(Middleware::history($history));
         $http = new HttpClient(['handler' => $stack]);
-        $service = new OpenAIService($http, 'key', 'model-x');
+        Config::getInstance()->set('OPENAI_MODEL', 'model-x');
+        $service = new OpenAIService($http, 'key');
         $result = $service->chat([['role'=>'user','content'=>'hi']], ['temperature' => 0]);
         $this->assertSame('ok', $result['choices'][0]['message']['content']);
         $this->assertCount(1, $history);
